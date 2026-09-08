693356fa45a60d8026baaff19f9faaf03acbd83a
João Vitor Hobus <69995779+miojo-dev@users.noreply.github.com>
2026-09-07 22:59:52 -0300

    feat/finalize graph object

--- Graph.cs
+++ Graph.cs
@@ -2,12 +2,32 @@ namespace Dijkstra_C_Sharp_Implementation;
 
 public class Graph
 {
+    private string Name { get; }
     private int Degree { get; }
-    private unsafe Vertex*[] conections { get; set; }
+    private Vertex[] Connections { get; set; }
 
-    public unsafe Graph(int degree)
+    public Graph(string name,int degree)
     {
+        this.Name = name;
         this.Degree = degree;
-        conections = new Vertex*[this.Degree];
+        Connections = new Vertex[this.Degree];
     }
+
+    public bool TryAddConnection(string name, Graph connectedGraph, int distance)
+    {
+        for (var i = 0; i < Connections.Length; i++)
+        {
+            if (Connections[i].GetName() == string.Empty)
+            {
+                Connections[i] = new Vertex(name, connectedGraph, distance);
+                return true;
+            }
+        }
+
+        return false;
+    }
+    
+    public string GetName() => Name;
+    public int GetDegree() => Degree;
+    public Vertex[] GetConnections() => Connections;
 }
--- Vertex.cs
+++ Vertex.cs
@@ -3,13 +3,17 @@ namespace Dijkstra_C_Sharp_Implementation;
 public struct Vertex
 {
     private string Name { get; }
-    private unsafe Graph* _end { get; }
+    private unsafe Graph* _next { get; }
     private int Distance { get; }
 
     public unsafe Vertex(string name, Graph connection, int distance)
     {
         Name = name;
-        _end = &connection;
+        _next = &connection;
         Distance = distance;
     }
+
+    public string GetName() => Name;
+    public unsafe Graph* GetEnd() => _next;
+    public int GetDistance() => Distance;
 }
