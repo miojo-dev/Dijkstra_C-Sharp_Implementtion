cca2c7e0c2a71893a96931237e5854aef047fd13
guilhermesmd <guilherme.dellagiustina@unidavi.edu.br>
2026-09-22 21:39:50 -0300

    feat/Initialize CostList Population

--- Inializers.cs
+++ Inializers.cs
@@ -2,6 +2,8 @@
 
 public static class Initializers
 {
+    public static int BlockedPathCost = 1000;
+    
     private static readonly string[] GraphNames =
     [
         "N1", "N2", "ND1", "ND2", "ND3", "ND4", "ND5", "C1", "C2", "C3", "C4", "SD1", "SD2", "SD3", "SD4", "SD5", "SD6",
@@ -36,13 +38,15 @@ public static class Initializers
         [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
     ];
 
-    private static readonly int[][] IncidencyList = new int[][]
-    {
-    };
+    private static readonly int[][] IncidencyList =
+    [
+    ];
 
-    private static readonly int[][] CostList = new int[][]
-    {
-    };
+    private static readonly int[][] CostList =
+    [
+        [BlockedPathCost, 10, BlockedPathCost, BlockedPathCost, BlockedPathCost, BlockedPathCost, BlockedPathCost, BlockedPathCost, BlockedPathCost, BlockedPathCost, BlockedPathCost, 18, BlockedPathCost, BlockedPathCost, BlockedPathCost, BlockedPathCost, 24, BlockedPathCost, BlockedPathCost, 28, BlockedPathCost, 26, BlockedPathCost, BlockedPathCost],
+        [10, BlockedPathCost, BlockedPathCost, BlockedPathCost, BlockedPathCost, BlockedPathCost, BlockedPathCost, BlockedPathCost, BlockedPathCost, BlockedPathCost, BlockedPathCost, 22, BlockedPathCost, BlockedPathCost, BlockedPathCost, BlockedPathCost, 18, BlockedPathCost, BlockedPathCost, 24, BlockedPathCost, 30, BlockedPathCost, BlockedPathCost],
+    ];
 
     public static void ResetGraphs()
     {
